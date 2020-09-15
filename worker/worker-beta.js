49cd2cddd141566fab462a6e14e9096e82830c55
Parveen Bhadoo <bhadoo@pm.me>
2020-09-15 09:36:09 +0530

    Update worker-beta.js

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -21,7 +21,10 @@ const authConfig = {
 	 */
 	"roots": [{
 		id: "root",
-		name: "Cloud Zero"
+		name: "Cloud Zero",
+		user: "",
+		pass: "",
+		protect_file_link: false
 	}],
 	/**
 	 * The number displayed on each page of the file list page. [Recommended setting value is between 100 and 1000];
