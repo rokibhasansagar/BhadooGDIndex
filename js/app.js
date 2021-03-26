fafdb9e750b3f7c2644fcc5070728dea0b3852d4
Parveen Bhadoo <admin@hashhackers.com>
2021-03-26 20:21:00 +0530

    fixed navbar

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ function nav(path) {
 	var model = window.MODEL;
 	var html = "";
 	var cur = window.current_drive_order || 0;
-	html += `<nav class="navbar navbar-expand-lg ${UI.dark_mode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}">
+	html += `<nav class="navbar navbar-expand-lg fixed-top ${UI.dark_mode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}">
   <a class="navbar-brand" href="/${cur}:/">${UI.logo_image ? '<img border="0" alt="'+UI.company_name+'" src="'+UI.logo_link_name+'" height="'+UI.height+'" width="'+UI.logo_width+'">' : UI.logo_link_name}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
