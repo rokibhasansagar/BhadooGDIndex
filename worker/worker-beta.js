f48da8522980283f1629e503a7c1c7bd7140b91c
Parveen Bhadoo <admin@hashhackers.com>
2021-04-23 18:15:29 +0530

    Update worker-beta.js

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -235,6 +235,7 @@ function html(current_drive_order = 0, model = {}) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <script src="https://cdn.plyr.io/${uiConfig.plyr_io_version}/plyr.polyfilled.js"></script>
+  <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
 </html>`;
 };
 
