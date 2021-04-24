a5afe8e55994828cd3865537178e09d974786049
Adnan Ahmad <viperadnan@gmail.com>
2021-04-24 12:29:14 +0530

    Added missing Head_md
    Added missing HEAD.md code without using card division

--- js/app.js
+++ js/app.js
@@ -212,6 +212,7 @@ function requestSearch(params, resultCallback) {
 function list(path) {
     var content = `
   <div class="container">${UI.fixed_header ?'<br>': ''}
+  <div id="head_md" style="display:none; padding: 20px 20px;"></div>
   <div class="card">
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb" id="folderne"><li class="breadcrumb-item"><a href="/">Home</a></li>&nbsp;->&nbsp;
