c7c7862e219d820e9047243b2d1c497f7cd13014
Parveen Bhadoo <admin@hashhackers.com>
2021-11-16 20:02:50 +0530

    Error Fix #33

--- worker/worker-beta-second-domain.js
+++ worker/worker-beta-second-domain.js
@@ -212,50 +212,6 @@ a{
 </html>
 `
 
-const asn_blocked = `<html>
-<head>
-<title>Access Denied</title>
-<link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-<style>
-body{
-    margin:0;
-    padding:0;
-    width:100%;
-    height:100%;
-    color:#b0bec5;
-    display:table;
-    font-weight:100;
-    font-family:Lato
-}
-.container{
-    text-align:center;
-    display:table-cell;
-    vertical-align:middle
-}
-.content{
-    text-align:center;
-    display:inline-block
-}
-.message{
-    font-size:80px;
-    margin-bottom:40px
-}
-a{
-    text-decoration:none;
-    color:#3498db
-}
-
-</style>
-</head>
-<body>
-<div class="container">
-<div class="content">
-<div class="message">Access Denied</div>
-</div>
-</div>
-</body>
-</html>`
-
 const SearchFunction = {
     formatSearchKeyword: function(keyword) {
         let nothing = "";
